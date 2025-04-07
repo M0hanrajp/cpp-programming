40837fd6d1d8e39aa9b5f619e7b6cc8bc0738c98
M0hanrajp <mohanrajp.github@gmail.com>
2025-04-07 06:37:15 +0530

    Cpp syntax part 1 * Basics of cpp program * Usage of namespace std * Print using cout * Using cout to print integers

---
+++ syntax/1_helloworld.cpp
@@ -0,0 +1,31 @@
+// #include <iostream> is a header file library that lets us work with input and output objects, such as cout
+#include <iostream>
+// using namespace std means that we can use names for objects and variables from the standard library.
+using namespace std;
+
+// main function
+int main(void) {
+    // cout (pronounced "see-out") is an object used together with the insertion operator (<<) to output/print text.
+    cout << "Hello, World!\n";
+    // The using namespace std line can be omitted and replaced with the std keyword, followed by the :: operator for some objects:
+    std::cout << "Hello, World!\n";
+    // The cout object, together with the << operator, is used to output values and print text.
+    cout << "Hello, "
+            "I'm learning cpp\n";
+    // end
+    return 0;
+}
+
+// blank line. C++ ignores white space. But we use it to make the code more readable.
+// C++ is case-sensitive: "cout" and "Cout" has different meaning.
+// Every C++ statement ends with a semicolon ;.
+// What happens if you omit "using namespace std" $ g++ -Wall -g helloworld.cpp
+/* helloworld.cpp: In function ‘int main()’:
+helloworld.cpp:9:5: error: ‘cout’ was not declared in this scope; did you mean ‘std::cout’?
+    9 |     cout << "Hello, World!\n";
+      |     ^~~~
+      |     std::cout
+In file included from helloworld.cpp:2:
+/usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here
+   61 |   extern ostream cout;          /// Linked to standard output
+      |                  ^~~~ */
---
+++ syntax/2_integers_and_arithmetic_with_cout.cpp
@@ -0,0 +1,22 @@
+// single line comments
+/*
+ * Mulit line comments
+ *
+ */
+#include <iostream>
+#include <ostream>
+
+int main(void) {
+    // You can also use another << operator and place the \n character after the text, like this:
+    std::cout << 3 << "\n";
+    // Two \n characters after each other will create a blank line:
+    std::cout << 4 * 3 << "\nInserting blankline\n\n";
+    // Another way to insert a new line, is with the endl manipulator:
+    std::cout << 3 * 3 << std::endl;
+    std::cout << 5 % 2 << std::endl;
+    return 0;
+}
+/* 
+\t	Creates a horizontal tab	
+\\	Inserts a backslash character (\)	
+\"	Inserts a double quote character */
