35ee349719090f9759c75855ff8f99a363481822
M0hanrajp <mohanrajp.github@gmail.com>
2025-06-30 22:52:36 +0530

    (C++): opertor overloading

--- syntax/025_class_constructors.cpp
+++ syntax/025_class_constructors.cpp
@@ -3,12 +3,19 @@
 * A constructor in C++ is a special method that is automatically called when an object of a class is created.
 * To create a constructor, use the same name as the class, followed by parentheses (): 
 * Note: The constructor has the same name as the class, it is always public, and it does not have any return value.
+*
+* rules for creating constructors
+* a. Only constructors can have the same name as the class.
+*   a.a this is also like special member function, but it does not have any return type, not even void.
+* b. They can be declared outside the class or inside the class.
+* c. they can have default parameters.
+* d. every constructor is a member function, but not every member function is a constructor.
 */
 
 #include <iostream>
 class calculatorDemo { // class
     public: // access specifier
-        int operandA; // attributes
+        int operandA; // attributes (these are not necessary)
         int operandB;
         int addition;
         int substraction;
@@ -16,8 +23,8 @@ class calculatorDemo { // class
         int divison;
         // constructor method
         calculatorDemo(int *a, int *b) { // constructor defined inside class
-            operandA = *a;
-            operandB = *b;
+            //operandA = *a; this is not necessary
+            //operandB = *b;
             addition = *a + *b;
             substraction = *a - *b;
             multiplication = *a * *b;
